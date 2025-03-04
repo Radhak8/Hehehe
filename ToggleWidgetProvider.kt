13c7e1a4ed1daa0b5491776b0d070d20c62a4e0c
Rᴀᴅʜᴀ Sɪɴɢʜ <133753917+Radhak8@users.noreply.github.com>
2025-03-04 18:26:43 +0530

    Create ToggleWidgetProvider.kt

---
+++ ToggleWidgetProvider.kt
@@ -0,0 +1,24 @@
+package com.example.togglemaster
+
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.widget.RemoteViews
+
+class ToggleWidgetProvider : AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        for (appWidgetId in appWidgetIds) {
+            val views = RemoteViews(context.packageName, R.layout.widget_layout)
+            val intent = Intent(context, MainActivity::class.java)
+            val pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
+            views.setOnClickPendingIntent(R.id.widgetToggleButton, pendingIntent)
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+}
